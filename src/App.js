33a22705a94ddb4124b2b55b2ba7363965609752
Dan Caragea <dancaragea@gmail.com>
2016-08-02 14:46:38 +0300

    auth0 styleguide boilerplate & overwrites

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
 import shouldPureComponentUpdate from 'react-pure-render/function';
+import Header from './Header';
+import styles from './App.css';
 
 class App extends Component {
 
   shouldComponentUpdate = shouldPureComponentUpdate;
 
   render() {
-    return <p>All good</p>;
+    return (
+      <div>
+        <Header />
+        <div className={classNames('container', styles.container)}>
+          <h1>Zendesk Subscriptions</h1>
+        </div>
+      </div>
+    );
   }
 }
 
---
+++ src/Header.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import classNames from 'classnames';
+import styles from './Header.css';
+
+function Header() {
+  return (
+    <header className={classNames(styles.header, 'site-header', 'clearfix')}>
+      <nav role="navigation" className="navbar navbar-default">
+        <div className="container">
+          <div className="navbar-header">
+            <button type="button" dataToggle="collapse" dataTarget="#navbar-collapse" className="navbar-toggle">
+              <span className="sr-only">Toggle navigation</span><span className="icon-bar"></span><span className="icon-bar"></span><span className="icon-bar"></span>
+            </button>
+            <h1 className="navbar-brand"><a href="/"><span>Auth0</span></a></h1>
+            <a href="https://auth0.com/jobs" className="no-basic hiring animated bounce hidden-sm hidden-xs hidden-md">We're hiring!</a></div>
+          <div id="navbar-collapse" className="collapse navbar-collapse">
+            <ul className="nav navbar-nav navbar-left no-basic">
+              <li className="li-why"><a href="https://auth0.com/why-auth0">Why Dan+Auth0</a></li>
+              <li className="li-how"><a href="https://auth0.com/how-it-works">How It Works</a>
+              </li>
+              <li className="li-pricing"><a href="https://auth0.com/pricing">Pricing</a></li>
+              <li className="dropdown"><span role="button" dataToggle="dropdown" className="btn-dro">More<i className="icon-budicon-460"></i></span>
+                <ul role="menu" ariaLabelledby="dLabel" className="dropdown-menu">
+                  <li><a href="https://auth0.com/lock">Lock</a></li>
+                  <li><a href="https://auth0.com/passwordless">Passwordless</a></li>
+                  <li><a href="https://auth0.com/wordpress">WordPress</a></li>
+                  <li className="divider"></li>
+                  <li><a href="https://auth0.com/about">About</a></li>
+                  <li><a href="https://auth0.com/blog">Blog</a></li>
+                  <li><a href="https://auth0.com/customers">Customers</a></li>
+                  <li><a href="https://auth0.com/partners">Partners</a></li>
+                  <li><a href="https://auth0.com/opensource">Open Source</a></li>
+                  <li><a href="https://auth0.com/jobs">Jobs</a></li>
+                </ul>
+              </li>
+            </ul>
+            <ul className="nav navbar-nav navbar-right">
+              <li className="li-docs no-basic"><a href="https://auth0.com/support">Help &amp; Support</a></li>
+              <li className="li-docs no-basic"><a href="https://auth0.com/docs">Documentation</a>
+              </li>
+              <li><a href="" className="signin-button login">Login</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
