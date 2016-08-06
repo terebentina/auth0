569021ddd4da5b27aac3d2e6b509f120c5307b9d
Dan Caragea <dancaragea@gmail.com>
2016-08-06 16:00:33 +0300

    Prepopulate redux store from localstorage if possible (autologin)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import classNames from 'classnames';
+import { autoLoginIfPossible } from './actions';
 import HeaderConnector from './components/HeaderConnector';
 import styles from './App.css';
 
-function App({ children }) {
-  return (
-    <div>
-      <HeaderConnector />
-      <div className={classNames('container', styles.container)}>
-        { children }
+class App extends Component {
+
+  componentWillMount() {
+    this.props.autoLoginIfPossible();
+  }
+
+  render() {
+    return (
+      <div>
+        <HeaderConnector />
+        <div className={classNames('container', styles.container)}>
+          { this.props.children }
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 App.propTypes = {
   children: PropTypes.any,
+  autoLoginIfPossible: PropTypes.func,
 };
 
-export default App;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ autoLoginIfPossible }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(App);
